c061b9204592a0244b24fe90fa8601a38532fdbd
mihaly1997 <kadar.mihaly2015@gmail.com>
2020-12-19 15:00:42 +0200

    js 1

--- functions.js
+++ functions.js
@@ -1,1 +1,28 @@
-function 
+console.log("sal")
+
+function printPeron (){
+    const tbody = document.querySelector(`#list tbody`);
+    tbody.innerHTML = `
+    <tr >
+    <td>
+        Mihaly
+    </td>
+    <td>
+        Kadar
+    </td>
+    <td>
+        <a href="https://github.com/mihaly1997"><img src="github.png" aalt="" width="20px"></a>
+    </td>
+</tr>`;
+}
+
+printPeron();
+
+fetch('persons.json')
+.then(res=> res.json())
+.then(data=>{
+    console.log(`data:`,data);
+
+    printPeron
+});
+
