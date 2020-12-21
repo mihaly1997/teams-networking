aab5009e744cd6d5f5326bcb2034f36b2d96e069
mihaly1997 <kadar.mihaly2015@gmail.com>
2020-12-21 21:28:18 +0200

    search

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
-console.log("sal")
 
-function insertPeron(persons){
+function insertPerson(persons){
     const tbody = document.querySelector('#list tbody');
     tbody.innerHTML = getPersonsHtml(persons); 
 }
@@ -21,10 +20,29 @@ function getPersonHtml (persons){
 </tr> `;
 }
 
+let allPersons = [];
 
 fetch('persons.json')
 .then(res=> res.json())
 .then(data=>{
-   insertPeron ( data)
+    allPersons= data;
+   insertPerson ( data)
 });
 
+function searchPersons(text){
+    text= text.toLowerCase();
+    console.warn("serch", text, allPersons );
+    return allPersons.filter(person => {
+        console.info({person});
+        return person.firstName.toLowerCase().indexOf(text) > -1 ||
+        person.lastName.toLowerCase().indexOf(text) > -1 ;
+
+    });
+}
+
+const search = document.getElementById('search')
+search.addEventListener("input", e => {
+    const text = e.target.value
+    const filtrate = searchPersons(text);
+    insertPerson(filtrate);
+})
