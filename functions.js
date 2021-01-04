54bba0338fbd7774481d91108a585a47e63b93e9
mihaly1997 <kadar.mihaly2015@gmail.com>
2021-01-04 21:30:11 +0200

    js nou

--- functions.js
+++ functions.js
@@ -1,4 +1,21 @@
-
+const API = {
+    CREATE: {
+        URL: "create.json",
+        METHOD: "GET" //POST
+    },
+    READ: {
+        URL: "team.json",
+        METHOD: "GET"
+    },
+    UPDATE: {
+       URL: "",
+        METHOD: "GET"
+    },
+    DELETE: {
+        URL: "delete.json",
+        METHOD: "GET"
+    }
+};
 function insertPerson(persons){
     const tbody = document.querySelector('#list tbody');
     tbody.innerHTML = getPersonsHtml(persons); 
@@ -17,18 +34,23 @@ function getPersonHtml (persons){
     <td>${persons.lastName}</td>
     <td><a href="https://github.com/${gitHub}">
     <img src="github.png" aalt="" width="20px"></a></td>
+    <td> <a hre="${API.DELETE.URL}?id=${persons.id}">&#10006</a>
+    </td>
 </tr> `;
 }
 
 let allPersons = [];
 
-fetch('persons.json')
-.then(res=> res.json())
-.then(data=>{
-    allPersons= data;
-   insertPerson ( data)
-});
-
+function loadList(){
+    fetch('persons.json')
+    .then(res=> res.json())
+    .then(data=>{
+        allPersons= data;
+       insertPerson ( data)
+    });
+    
+}
+loadList()
 function searchPersons(text){
     text= text.toLowerCase();
     console.warn("serch", text, allPersons );
@@ -45,4 +67,32 @@ search.addEventListener("input", e => {
     const text = e.target.value
     const filtrate = searchPersons(text);
     insertPerson(filtrate);
-})
+});
+
+function vaveTeamMember() {
+    const firstName = document.querySelector("input[name=firstName").value;
+    const lastName = document.querySelector("input[name=lastName").value;
+    const gitHub = document.querySelector("input[name=gitHub").value;
+    
+    const person = (
+        firstName, lastName,  gitHub
+    );
+    console.info('saving...', person, JSON.stringify(person))
+
+    fetch(API.CREATE.URL,{
+        method: API.CREATE.METHOD,
+        body:API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
+    })
+    .then(res => res.json())
+    .then(r => {
+        if (r.success){
+            alert()
+            loadList()
+        }
+    });
+};
+
+const savebtn = document.querySelector("#list button"); 
+savebtn.addEventListener("click", () => {
+    vaveTeamMember();
+});
